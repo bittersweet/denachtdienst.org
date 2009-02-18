3cb807429e3ca6f120bc06dbe95df4fa0f507b51
Mark Mulder <markmulder@gmail.com>
2009-02-18 15:32:07 +0100

    Changed the default route from /session/new to /login

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,14 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  include AuthenticatedSystem
+
+  #this override redirects the user to /login instead of /session/new
+  def access_denied
+    alias new_session_path login_path
+    super
+  end
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'a45e3e72714cdd7363a0c8954c6b841c'
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,9 @@
 class ArtistsController < ApplicationController
+
+  before_filter :login_required
+
   def index
+    @artist = Time.now
   end
 
 end
