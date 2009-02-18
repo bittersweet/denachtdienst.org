7024d5205a51cd339d4fb01a9a50082d658be27b
Mark Mulder <markmulder@gmail.com>
2009-02-18 19:06:55 +0100

    Added tracks as a recourse to routes.rb so I can work with the paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.resources :users, :has_many => :tracks
+  map.resources :tracks
   map.resource :session
 
   # The priority is based upon order of creation: first created -> highest priority.
