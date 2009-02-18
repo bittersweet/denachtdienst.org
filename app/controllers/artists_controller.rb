03eb386b058900bf6fdcc40ab90b7759e13bc1bd
Mark Mulder <markmulder@gmail.com>
2009-02-18 15:58:57 +0100

    Now showing user information on the main page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,6 +3,11 @@ class ArtistsController < ApplicationController
   before_filter :login_required
 
   def index
+    
+    if logged_in?
+      @profile = current_user
+    end
+    
     @artist = Time.now
   end
 
