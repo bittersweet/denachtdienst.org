9e3b82884458f066655a4433de47393131ec9e9e
Mark Mulder <markmulder@gmail.com>
2009-02-18 18:13:49 +0100

    You can now add tracks via /users/:user_id/tracks/new

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -7,4 +7,10 @@ class TracksController < ApplicationController
     @track = Track.new
   end
 
+  def create
+    @user = User.find(params[:user_id])
+    @user.tracks.create :name => params[:track][:name]
+    redirect_to :controller => "tracks", :action => "index"
+  end
+
 end
