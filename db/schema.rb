e683a8f4dd695b075b6246f767b68f8fc9790a2a
Mark Mulder <markmulder@gmail.com>
2009-02-18 19:38:52 +0100

    Migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090218162446) do
+ActiveRecord::Schema.define(:version => 20090218183241) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "roles", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
 
   create_table "tracks", :force => true do |t|
     t.string   "name"
