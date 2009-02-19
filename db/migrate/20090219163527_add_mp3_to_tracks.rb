6bed8a1e495db498848bdc9d0529232fa4e144f2
Mark Mulder <markmulder@gmail.com>
2009-02-19 17:41:33 +0100

    The migration was not complete.

--- db/migrate/20090219163527_add_mp3_to_tracks.rb
+++ db/migrate/20090219163527_add_mp3_to_tracks.rb
@@ -1,9 +1,13 @@
 class AddMp3ToTracks < ActiveRecord::Migration
   def self.up
-    add_column :tracks, :mp3, :string
+    add_column :tracks, :mp3_file_name, :string
+    add_column :tracks, :mp3_content_type, :string
+    add_column :tracks, :mp3_file_size, :string
   end
 
   def self.down
-    remove_column :tracks, :mp3
+    remove_column :tracks, :mp3_file_size
+    remove_column :tracks, :mp3_content_type
+    remove_column :tracks, :mp3_file_name
   end
 end
