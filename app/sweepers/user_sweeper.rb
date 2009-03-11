a1fcfbbd2b3c8ccb820946f1dc8e8d7d10571761
Mark Mulder <markmulder@gmail.com>
2009-03-11 18:21:41 +0100

    The index page now gets expired if the user is updated.

--- app/sweepers/user_sweeper.rb
+++ app/sweepers/user_sweeper.rb
@@ -12,7 +12,7 @@ class UserSweeper < ActionController::Caching::Sweeper
   
   def clear_users_cache(user)
     expire_page :controller => :users, :action => :show, :id => user
-
+    expire_page "/index.html"
   end
   
 end
