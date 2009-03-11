872f84e01fb83fdf023856472523bc17536be054
Mark Mulder <markmulder@gmail.com>
2009-03-11 18:05:38 +0100

    Paperclip errors now get logged to apache's error log.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 # Imagemagick path for Paperclip image processing
 Paperclip.options[:command_path] = '/usr/local/bin/ImageMagick-6.4.8/bin'
-Paperclip.options[:swallow_stderr] = 'false'
+Paperclip.options[:swallow_stderr] = false
