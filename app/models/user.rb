9d6e78dba4874065f6b20775ecdf2d333131f40e
Mark Mulder <markmulder@gmail.com>
2009-03-12 19:01:56 +0100

    Only check the invitation_id on create otherwise we can't edit the user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
   
-  validates_presence_of     :invitation_id, :message => "is required"
+  validates_presence_of     :invitation_id, :on => :create, :message => "is required"
   validates_uniqueness_of   :invitation_id
   
   # HACK HACK HACK -- how to do attr_accessible from here?
