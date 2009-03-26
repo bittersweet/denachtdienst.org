45f4da2ce4ec5ce3e9d6a8e98d80bace5f8bf694
Mark Mulder <markmulder@gmail.com>
2009-03-26 16:48:22 +0100

    Modified the Capistrano recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,52 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
+set :application, "denachtdienst.org"
+set :user, "deploy"
+set :port, 30000
+set :repository,  "git@github.com:bittersweet/denachtdienst.org.git"
+set :scm, "git"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+ssh_options[:forward_agent] = true
+ssh_options[:username] = "deploy"
+# ssh_options[:host_key] = %w("/Users/markmulder/.ssh/id_rsa")
+set :use_sudo, false
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :deploy_to, "/home/deploy/sites/#{application}"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "213.163.66.221"
+role :web, "213.163.66.221"
+role :db,  "213.163.66.221", :primary => true
+
+set :deploy_via, "remote_cache"
+
+desc "After updating code we need to populate a new database.yml"
+task :after_update_code, :roles => :app do
+  require "yaml"
+  set :production_database_password, proc { Capistrano::CLI.password_prompt("Production database remote Password : ") }
+
+  buffer = YAML::load_file('config/database.example.yml')
+  # get ride of unneeded configurations
+  buffer.delete('test')
+  buffer.delete('development')
+  
+  # Populate production element
+  buffer['production']['adapter'] = "mysql"
+  buffer['production']['database'] = "dndorg_prod"
+  buffer['production']['username'] = "dnd"
+  buffer['production']['password'] = production_database_password
+  buffer['production']['host'] = "localhost"
+  
+  put YAML::dump(buffer), "#{release_path}/config/database.yml", :mode => 0664
+end
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, "passenger:restart"
+
+namespace :deploy do
+  %w(start restart).each { |name| task name, :roles => :app do passenger.restart end }
+end
