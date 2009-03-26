95c8ecc64904b2ccc22f540b066414437af53d4f
Mark Mulder <markmulder@gmail.com>
2009-03-26 16:48:04 +0100

    Only use this ImageMagick path in development.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,5 @@
 # Imagemagick path for Paperclip image processing
-Paperclip.options[:command_path] = '/usr/local/bin/ImageMagick-6.4.8/bin'
+if RAILS_ENV == "development"
+  Paperclip.options[:command_path] = '/usr/local/bin/ImageMagick-6.4.8/bin'
+end
 Paperclip.options[:swallow_stderr] = false
