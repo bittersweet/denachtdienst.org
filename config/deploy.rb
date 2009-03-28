43a6d5194a5dc4a7ea15c7acacece46d90cce257
Mark Mulder <markmulder@gmail.com>
2009-03-28 19:22:35 +0100

    Modified the deploy, now copies over database.yml instead of asking for input every deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,46 +4,31 @@ set :user, "deploy"
 set :port, 30000
 set :repository,  "git@github.com:bittersweet/denachtdienst.org.git"
 set :scm, "git"
+set :deploy_to, "/home/deploy/sites/#{application}"
+set :deploy_via, "remote_cache"
 
 ssh_options[:forward_agent] = true
 ssh_options[:username] = "deploy"
-# ssh_options[:host_key] = %w("/Users/markmulder/.ssh/id_rsa")
 set :use_sudo, false
 
-set :deploy_to, "/home/deploy/sites/#{application}"
+role :app, application
+role :web, application
+role :db,  application, :primary => true
 
-role :app, "213.163.66.221"
-role :web, "213.163.66.221"
-role :db,  "213.163.66.221", :primary => true
+namespace :deploy do	
 
-set :deploy_via, "remote_cache"
+  after "deploy:update_code", "deploy:link_production_db", "deploy:package_assets"
 
-desc "Populate database.yml and package CSS/JS"
-task :after_update_code, :roles => :app do
-  require "yaml"
-  set :production_database_password, proc { Capistrano::CLI.password_prompt("Production database remote Password : ") }
+  desc "Link in the production database.yml"
+  task :link_production_db do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
 
-  buffer = YAML::load_file('config/database.example.yml')
-  # get ride of unneeded configurations
-  buffer.delete('test')
-  buffer.delete('development')
-  
-  # Populate production element
-  buffer['production']['adapter'] = "mysql"
-  buffer['production']['database'] = "dndorg_prod"
-  buffer['production']['username'] = "dnd"
-  buffer['production']['password'] = production_database_password
-  buffer['production']['host'] = "localhost"
-  
-  put YAML::dump(buffer), "#{release_path}/config/database.yml", :mode => 0664
+  desc "Package CSS and JS files"
+  task :package_assets do
+    run "cd #{release_path} && rake asset:packager:build_all"
+  end
 
-  #package css and js
-  run "cd #{release_path} && rake asset:packager:build_all"
-  
-end
-
-
-namespace :deploy do
   desc "Restarting Passenger with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
