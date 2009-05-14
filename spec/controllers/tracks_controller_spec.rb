570ee19c8420c8d4ec6d4078c44402e8d542c615
Mark Mulder <markmulder@gmail.com>
2009-05-14 12:38:10 +0200

    Generated RSpec for Tracks controller.

---
+++ spec/controllers/tracks_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe TracksController do
+
+  #Delete this example and add some real ones
+  it "should use TracksController" do
+    controller.should be_an_instance_of(TracksController)
+  end
+
+end
