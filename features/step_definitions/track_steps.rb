f52bf32eaae473a9bd3f909610fd356f047bb1b8
Mark Mulder <markmulder@gmail.com>
2009-05-14 13:06:18 +0200

    Wrote my first passing RSpec tests. Test for file upload now works and added a mp3 fixture file.

--- features/step_definitions/track_steps.rb
+++ features/step_definitions/track_steps.rb
@@ -1,14 +1,14 @@
-Given /^I have tracks titled (.+)$/ do |titles|
-  titles.split(', ').each do |title|
-    Track.create!(:name => title, :mp3_file_name => "test.mp3" )
-  end
-end
-
 Given /^I am logged in as an Admin$/ do
   visit login_path
   fill_in("login", :with => "admin")
   fill_in("password", :with => "password")
   click_button("Log in")
+end
+
+Given /^I have tracks titled (.+)$/ do |titles|
+  titles.split(', ').each do |title|
+    Track.create!(:name => title, :user_id => "1", :mp3_file_name => "test.mp3" )
+  end
 end
 
 Then /^I should see my account details$/ do
--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -1,10 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe TracksController do
+describe TracksController, "when creating a new track" do
+  
+  before( :each ) do
+    @track = Track.new
+  end
+  
+  it "should be invalid without a name" do
+    @track.should_not be_valid
+  end
+  
+  it "should be invalid without a track" do
+    @track.name = "testtrack"
+    @track.should_not be_valid
+  end
 
-  #Delete this example and add some real ones
-  it "should use TracksController" do
-    controller.should be_an_instance_of(TracksController)
+  it "should be valid with a track but without a name" do
+    @track = Track.new(:mp3 => fixture_file_upload('dilla.mp3', 'audio/mpeg'))
+    @track.stub!(:save_attached_files).and_return true
+    @track.save.should_not be_true
+  end
+  
+  it "should be valid with a track" do
+    @track = Track.new(:name => "test", :mp3 => fixture_file_upload('dilla.mp3', 'audio/mpeg'))
+    @track.stub!(:save_attached_files).and_return true
+    @track.save.should be_true
   end
 
 end
