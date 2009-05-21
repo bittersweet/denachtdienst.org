b7faf14dfde7805f08b1af20fcfda28a58040ecd
Mark Mulder <markmulder@gmail.com>
2009-05-21 18:26:53 +0200

    Forgot to change something in the last commit.

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,6 +1,6 @@
 module TracksHelper
   
-  def new_asset_path_with_session_information
+  def create_track_path_with_session_information
     session_key = ActionController::Base.session_options[:key]
     tracks_path(session_key => cookies[session_key], request_forgery_protection_token => form_authenticity_token)
   end  
