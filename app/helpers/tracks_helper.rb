6f8734e122bc43b8775db8be38707345bf692d0b
Mark Mulder <markmulder@gmail.com>
2009-05-21 18:24:29 +0200

    Made changes to the session store.

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,8 +1,7 @@
 module TracksHelper
   
-  def create_track_path_with_session_information
-    session_key = ActionController::Base.session_options[:session_key]
+  def new_asset_path_with_session_information
+    session_key = ActionController::Base.session_options[:key]
     tracks_path(session_key => cookies[session_key], request_forgery_protection_token => form_authenticity_token)
-  end
-  
+  end  
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,12 +4,13 @@
 # If you change this key, all old sessions will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
+
 ActionController::Base.session = {
-  :session_key => '_denachtdienst.org_session',
+  :key     => '_denachtdienst.org_session',
   :secret      => '6d473488da377ac2343099e580abc02d35c2ee4d2802a1fcab289ccb99d4ae87c543b7460fab45003df93ba08450d9e8f33589de2aaec96fabd9284641601e21'
 }
 
-ActionController::Dispatcher.middleware.use FlashSessionCookieMiddleware, ActionController::Base.session_options[:session_key]
+ActionController::Dispatcher.middleware.use FlashSessionCookieMiddleware, ActionController::Base.session_options[:key]
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
